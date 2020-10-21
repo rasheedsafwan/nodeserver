28b277db4024017c13e777994d19e8b0552e6b4b
rasheedsafwan <rasheedsafwan23@gmail.com>
2020-10-21 23:50:02 +0530

    changes made

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { response } = require('express')
 const express = require('express')
 const app = express()
-const port = 80
+const port = 443
 
 app.get('/', function (req, res)  {
   res.send('GET,future work: connect with database and READ data')
